330761542905d346ac8c6394f5412ef03082cbe8
Brent F <feuerbacherb@gmail.com>
2020-04-16 20:22:57 -0500

    set up to save in array of local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,28 @@ var momentEl = moment().hours(Number);
 var blkTime = $(".container");
 var lstTime = [];
 var idTime = [];
+var txtAreas = [];
+var schedule = [];
 
+
+// set the current date at the top of the page
 today.innerHTML = momentEl.format("dddd") + ", " + momentEl.format("MMMM Do YYYY");
 
+// check to see if a localStorage of schedule exists and either create new or populate the rows
+var loadInfo = function() {
+   txtAreas = JSON.parse(localStorage.getItem("schedule"));
+
+   if (!txtAreas) {
+      txtAreas = [];
+   };
+
+   console.log(txtAreas.length);
+
+   for (var i = 0; i < 10; i++) {
+      //console.log(txtAreas[i]);
+      $("textarea")[i].value = txtAreas[i];
+   }
+}
 
 // use a for loop to add text to the hours and get ready to set IDs
 var createTime = function() {
@@ -29,6 +48,7 @@ var createTime = function() {
    //console.log(idTime);
 }
 
+// create rows, textareas, and buttons using for loop
 var createTimeBlocks = function() {
    for (var i = 0; i < lstTime.length; i++) {
       var theRow = $("<div>")
@@ -47,19 +67,35 @@ var createTimeBlocks = function() {
          .addClass("far")
          .addClass("fa-save");
 
+      // add the row to the container
       blkTime.append(theRow);
 
+      // add the hour to the front of the row
       lftDiv.text(lstTime[i]);
       theRow.append(lftDiv);
 
+      // add the textarea
       midDiv.text();
       theRow.append(midDiv);
 
+      // add buttons to the rows
       rgtDiv.text();
       theRow.append(rgtDiv);
       
       //console.log(theRow);
    }
+}
+
+// loop through all of the textareas and save the information to an array in localStorage
+var saveInfo = function() {
+   console.log("saveInfo started");
+   console.log($(this));
+   var counter = 0;
+   for (var i = 0; i < 10; i++) {
+      txtAreas[i] = $("textarea")[i].value;
+   }
+   console.log(txtAreas);
+   localStorage.setItem("schedule", JSON.stringify(txtAreas));
 }
 
 var hrUpdater = function () {
@@ -80,9 +116,17 @@ var hrUpdater = function () {
    });
 }
 
+// these functions need to be in this order or it will break
 createTime();
 createTimeBlocks();
+loadInfo();
 hrUpdater();
 
+// button event handler to save the information
+$("button").on("click", function(event) {
+   //console.log("click event started");
+   //console.log(event.target);
+   saveInfo();
+});
 
 var updTime = setInterval(hrUpdater, 900000);
